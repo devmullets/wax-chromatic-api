b30dcf183c5394bb0ce34b94badfc591dee3eb28
devmullets <devmullets@gmail.com>
2020-07-07 07:49:55 -0400

    logic for displaying user collections

--- app/controllers/api/v1/artist_albums_controller.rb
+++ app/controllers/api/v1/artist_albums_controller.rb
@@ -1,6 +1,10 @@
 module Api
   module V1
     class ArtistAlbumsController < ApplicationController
+      def index
+        artist_albums = ArtistAlbum.all
+        render json: artist_albums
+      end
     end
     
   end
