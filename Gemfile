3e36b5feee4eaa1dc16780a59af9e1378d7e0761
denvermullets <devmullets@gmail.com>
2020-08-06 13:54:20 -0400

    :sparkles: finish oauth login

--- Gemfile
+++ Gemfile
@@ -42,6 +42,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'rest-client'
 gem 'json'
-gem 'faraday'
 # for hiding api keys
 gem 'dotenv-rails'
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,6 @@ module Api
   module V1
     class UsersController < ApplicationController
       def index
-        require 'json'
         # temporarily having the oauth token response in just a general url
 
         discogs_key = ENV["DISCOGS_KEY"]
@@ -19,7 +18,13 @@ module Api
           # byebug
           req_token = CGI.parse(url.body)
           oauth_url = "https://discogs.com/oauth/authorize?oauth_token=#{req_token['oauth_token'][0]}"
-          url_json = { 'discogs' => oauth_url }
+          
+          url_json = { 
+            'discogs' => oauth_url,
+            'oauth_token' => req_token['oauth_token'][0],
+            'oauth_token_secret' => req_token['oauth_token_secret'][0]
+          }
+
           render json: url_json
       end 
 
@@ -27,43 +32,48 @@ module Api
       end 
       
       def get_user 
-        require 'faraday'
         # last step in oauth verification, will also check to see if user is in db
+        require "uri"
+        require "net/http"
 
         discogs_key = ENV["DISCOGS_KEY"]
-        discogs_secret = ENV["DISCOGS_SECRET"]
+        discogs_secret = ENV["DISCOGS_SECRET"] + '&' + params[:oauth_token_secret]
+        discogs_secret_no_token = ENV["DISCOGS_SECRET"]
         oauth_token = params[:oauth_token].to_s
         oauth_verifier = params[:oauth_verifier].to_s
         time = Time.now.to_i.to_s
+        url = URI("https://api.discogs.com/oauth/access_token")
+        # 
+        # possible refactor to match code using RestClient, previously wasn't working
+        # 
+        https = Net::HTTP.new(url.host, url.port)
+        https.use_ssl = true
+        request = Net::HTTP::Post.new(url)
+        request["Authorization"] = "OAuth oauth_consumer_key=\"#{discogs_key}\", oauth_nonce=\"#{time}\", oauth_token=\"#{oauth_token}\", oauth_signature=\"#{discogs_secret}\", oauth_signature_method=\"PLAINTEXT\", oauth_timestamp=\"#{time}\", oauth_verifier=\"#{oauth_verifier}\""
+        request["Content-Type"] = "application/x-www-form-urlencoded"
+        request["User-Agent"] = "WaxChromatics/v0.1 +https://waxchromatics.com"
+        
+        response = https.request(request)
+        req_token = CGI.parse(response.read_body)
 
-        # Content-Type: application/x-www-form-urlencoded
-        # Authorization:
-        #   OAuth oauth_consumer_key="your_consumer_key",
-        #   oauth_nonce="random_string_or_timestamp",
-        #   oauth_token="oauth_token_received_from_step_2"
-        #   oauth_signature="your_consumer_secret&",
-        #   oauth_signature_method="PLAINTEXT",
-        #   oauth_timestamp="current_timestamp",
-        #   oauth_verifier="users_verifier"
-        # User-Agent: some_user_agent
-
-
-        # authorization_header = "OAuth oauth_consumer_key=#{discogs_key}, oauth_nonce=#{time}, oauth_token=#{oauth_token}, oauth_signature=#{discogs_secret}&, oauth_signature_method=PLAINTEXT, oauth_timestamp=#{time}, oauth_verifier=#{oauth_verifier}"
-        authorization_header = "OAuth oauth_consumer_key=#{discogs_key}, oauth_nonce=#{time}, oauth_token=#{oauth_token}, oauth_signature=#{discogs_secret + '&'}, oauth_signature_method=PLAINTEXT, oauth_timestamp=#{time}, oauth_verifier=#{oauth_verifier}"
-        url = ::RestClient::Request.execute(method: :post, url: "https://api.discogs.com/oauth/access_token", 
+        perm_oauth_token = req_token['oauth_token'][0]
+        perm_oauth_token_secret = req_token['oauth_token_secret'][0]
+        # 
+        # now to test that we have OAuth access by getting user identity
+        # 
+        user_info = ::RestClient::Request.execute(method: :get, url: "https://api.discogs.com/oauth/identity", 
           headers: {
             'Content-Type': 'application/x-www-form-urlencoded', 
-            # 'Authorization': "OAuth oauth_consumer_key=#{discogs_key}, oauth_nonce=#{time}, oauth_token=#{oauth_token}, oauth_signature=#{discogs_secret}&, oauth_signature_method=PLAINTEXT, oauth_timestamp=#{time}, oauth_verifier=#{oauth_verifier}",
-            'Authorization': authorization_header,
-            'User-Agent': "WaxChromatics/v0.1 +https://waxchromatics.com",
+            'Authorization': "OAuth oauth_consumer_key=#{discogs_key}, oauth_nonce=#{time}, oauth_signature=#{discogs_secret_no_token + '&' + perm_oauth_token_secret}, oauth_signature_method=PLAINTEXT, oauth_timestamp=#{time}, oauth_token=#{perm_oauth_token}",
+            'User-Agent': "WaxChromatics/v0.1 +https://waxchromatics.com"
             })
-
-        byebug
-        puts 'hi'
-
-        parsed_url = JSON.parse(url)
-        render json: parsed_url
-
+        # byebug
+        # 
+        # need to create user in table once logged in
+        # 
+        # user_info_json = CGI.parse(user_info.body)
+        user_info_json = JSON.parse(user_info)
+        render json: user_info_json
       end
 
     end
