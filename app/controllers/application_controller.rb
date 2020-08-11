9326a6ed4b2329500bc793ca7e139a9d9a0a62dc
denvermullets <devmullets@gmail.com>
2020-08-11 08:58:53 -0400

    :construction: able to use create new waxChromatics user, login, verify w/JWT. will need to rewrite all requests to api now to require auth header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,45 @@
 class ApplicationController < ActionController::API
+  before_action :authorized
+ 
+  
+  def encode_token(payload)
+    chromatics_key = ENV["WAX_KEY"]
+    discogs_key = ENV["DISCOGS_KEY"]
+    # should store secret in env variable
+    JWT.encode(payload, chromatics_key)
+  end
+  
+  def auth_header
+    # { Authorization: 'Bearer <token>' }
+    request.headers['Authorization']
+  end
+  
+  def decoded_token
+    chromatics_key = ENV["WAX_KEY"]
+    if auth_header
+      token = auth_header.split(' ')[1]
+      # header: { 'Authorization': 'Bearer <token>' }
+      begin
+        JWT.decode(token, chromatics_key, true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+ 
+  def current_user
+    if decoded_token
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
+ 
+  def logged_in?
+    !!current_user
+  end
+ 
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_112932) do
+ActiveRecord::Schema.define(version: 2020_08_10_205022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -185,6 +185,8 @@ ActiveRecord::Schema.define(version: 2020_08_07_112932) do
     t.string "discogs_id"
     t.string "oauth_token"
     t.string "oauth_token_secret"
+    t.string "password_digest"
+    t.string "email"
   end
 
   create_table "wantlist_albums", force: :cascade do |t|
