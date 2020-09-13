0407e54351b841cb04ad32c7bc1a46a39ec1d159
denvermullets <devmullets@gmail.com>
2020-09-13 14:17:01 -0400

    :rocket: update app name for new server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"
 
-set :application, "wax-chromatics-api"
+set :application, "api.waxchromatics.com"
 set :repo_url, "git@github.com:denvermullets/wax-chromatic-api.git"
 
 # Deploy to the user's home directory
